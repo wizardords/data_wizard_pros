8796d6aef7cab7db13d6ec3587a1d7a5bec356c9
wizardords <wizardordinals@gmail.com>
2024-08-07 08:42:09 -0400

    im just here

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,12 @@
-require('require-dir')('./gulp_tasks', {recurse: true});
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+// Define a task
+gulp.task('sass', function () {
+    return gulp.src('./src/scss/**/*.scss')  // Adjust this path as needed
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css'));      // Adjust this path as needed
+});
+
+// Default task
+gulp.task('default', gulp.series('sass'));
